3d9d2f9d644c3bb283f29acb53439529e44e170d
KAIF <kmo57787@gmail.com>
2026-01-14 10:15:12 +0530

    Project Uploaded

---
+++ super_admin/views.py
@@ -0,0 +1,118 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'super_admin/index.html')
+
+def signup(request):
+    pass
+
+def Login(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
+def signup(request):
+    pass
+
